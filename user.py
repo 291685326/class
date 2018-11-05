9a6c7106bed973ce7f224a7f215384903aa915ee
291685326 <291685326@qq.com>
2018-11-05 21:35:25 +0800

    change the class to a single class.

--- user.py
+++ user.py
@@ -1,4 +1,14 @@
 class User:
-    def __init__(self,name):
-        self._name=name
-
+    _instance = None
+    _first = True	
+    def __init__(self,name, age, sex):
+        if self._first :
+            self.name = name
+            self.age = age
+	    self.sex = sex
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = object.__new__(cls)
+            return cls._instance
+	else:
+	    return cls._instance
